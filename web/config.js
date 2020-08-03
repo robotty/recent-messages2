13dec6e84573acc0ff0d9cf6f43155d63781e28e
Ruben Anders <ruben.anders@robotty.de>
2020-08-03 12:13:13 +0200

    Updated web config to real production values

--- web/config.js
+++ web/config.js
@@ -1,11 +1,10 @@
 module.exports = {
     "client_id": "nld8y6rt7f5u7l1xuq4eni8pzp8mjo",
     "redirect_uri": "https://recent-messages.robotty.de/authorized",
-    "provider_info": "randers",
-    // human readable strings for the home page
+    // human readable strings for the home page and API documentation
     "messages_expire_after": "24 hours",
     "channels_expire_after": "24 hours",
-    "max_buffer_size": "500",
+    "max_buffer_size": "200",
     "github_link": "https://github.com/robotty/recent-messages2",
     // used for both the documentation as well as the actual API calls made by the web app. Don't include a trailing slash
     "api_base_url": "https://recent-messages.robotty.de/api/v2",
