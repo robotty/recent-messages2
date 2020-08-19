779309d02ef5cbb6f50fcfb271fc68cc9fb50d9f
Ruben Anders <ruben.anders@robotty.de>
2020-08-19 21:33:28 +0200

    Add id="donate" anchor to webpage

--- web/src/static.tsx
+++ web/src/static.tsx
@@ -111,7 +111,7 @@ export function Home() {
             </p>
         </section>
         <section>
-            <h4>Donate</h4>
+            <h4 id="donate">Donate</h4>
             <p>I made and run this project in my free time, and I don't want to collect money for doing that.
                 However <em>running</em> the service requires a server, which is not free. I currently pay about
                 26€ per month to run this service (that's just the server costs). If you are feeling generous, you can
