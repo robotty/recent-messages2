67328ab169f898bda542d1541d4a75bb5f3d6470
Ruben Anders <ruben.anders@robotty.de>
2020-08-25 23:43:07 +0200

    reformat script now supports --check parameter

--- scripts/reformat.sh
+++ scripts/reformat.sh
@@ -2,5 +2,15 @@
 
 set -e
 
-./web/node_modules/.bin/prettier --write --ignore-path ".gitignore" "**/*.md" "**/*.js" "**/*.tsx" "**/*.yml" "**/*.json"
-cargo fmt
+if [ "${1-}" = "--check" ]; then
+  echo "Checking formatting..."
+  PRETTIER_OPTIONS="--check"
+  CARGO_FMT_OPTIONS="-- --check"
+else
+  echo "Reformatting..."
+  PRETTIER_OPTIONS="--write"
+  CARGO_FMT_OPTIONS=""
+fi
+
+./web/node_modules/.bin/prettier $PRETTIER_OPTIONS --ignore-path ".gitignore" "**/*.md" "**/*.js" "**/*.tsx" "**/*.yml" "**/*.json"
+cargo fmt $CARGO_FMT_OPTIONS
