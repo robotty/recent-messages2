36667d1e9e39867e7342a062c4e524f767e21906
Ruben Anders <ruben.anders@robotty.de>
2026-06-08 21:48:23 +0200

    add comment

--- migrations/V5__columnstore.sql
+++ migrations/V5__columnstore.sql
@@ -1,2 +1,2 @@
 ALTER TABLE message SET (timescaledb.segmentby = 'channel_login');
-CALL add_columnstore_policy('message', after => INTERVAL '1 hour');
+CALL add_columnstore_policy('message', after => INTERVAL '1 hour'); -- same as chunk size in V4__timescaledb.sql
